aca3df9c7e5cf572dad20cf1c2a44516b2a2e57c
Arnab <royk43886@gmail.com>
2026-02-01 12:43:48 +0530

    Finding maximum in array
    Implement method to find the largest number in an array

---
+++ LargestArray.java
@@ -0,0 +1,12 @@
+class LargestArray {
+    public static int largest(int[] arr) {
+        // code here
+        int maxi = Integer.MIN_VALUE;
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i] > maxi)
+                maxi = arr[i];
+        }
+        return maxi;
+    }
+}
