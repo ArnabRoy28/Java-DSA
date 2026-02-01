604914b9688732043f877894217d351ea93efb8d
Arnab <royk43886@gmail.com>
2026-02-01 12:41:22 +0530

    Union of Arrays with Duplicate

---
+++ UniArray.java
@@ -0,0 +1,15 @@
+class UniArray {
+    public static ArrayList<Integer> findUnion(int[] a, int[] b) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int x : a) {
+            set.add(x);
+        }
+
+        for (int x : b) {
+            set.add(x);
+        }
+
+        return new ArrayList<>(set);
+    }
+}
